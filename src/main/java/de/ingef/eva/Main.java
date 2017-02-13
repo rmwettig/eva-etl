735a6b876cb0db2b1b297e9bab85c9f1245c7474
MW <martin.wettig@ingef.de>
2017-02-13 16:43:56 +0100

    change project structure

--- src/main/java/de/ingef/eva/Main.java
+++ src/main/java/de/ingef/eva/Main.java
@@ -1,4 +1,5 @@
 package de.ingef.eva;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
--- src/main/java/de/ingef/eva/processor/CleanRowsResultProcessor.java
+++ src/main/java/de/ingef/eva/processor/CleanRowsResultProcessor.java
@@ -1,7 +1,6 @@
 package de.ingef.eva.processor;
 
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;

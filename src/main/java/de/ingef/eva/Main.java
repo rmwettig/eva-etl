294e6f43806e5a9d149a15f015088676377c1c89
MW <martin.wettig@ingef.de>
2017-02-15 10:27:55 +0100

    remove test code + add query creation

--- src/main/java/de/ingef/eva/Main.java
+++ src/main/java/de/ingef/eva/Main.java
@@ -11,13 +11,9 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
 import de.ingef.eva.configuration.Configuration;
 import de.ingef.eva.configuration.ConfigurationReader;
+import de.ingef.eva.configuration.DatabaseQueryConfiguration;
 import de.ingef.eva.configuration.JsonConfigurationReader;
 import de.ingef.eva.processor.CleanRowsResultProcessor;
 import de.ingef.eva.processor.ResultProcessor;
@@ -31,18 +27,6 @@ public class Main {
 		final String configFilePath = args[0];
 		if(!configFilePath.isEmpty()){
 
-			ObjectMapper mapper = new ObjectMapper();
-			try {
-				JsonNode config = mapper.readTree(configFilePath);
-				int startYear = config.path("databases").path("startYear").asInt();
-				System.out.println("startyear: " + startYear);
-			} catch (JsonProcessingException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
 			ConfigurationReader configReader = new JsonConfigurationReader();
 			Configuration configuration = configReader.ReadConfiguration(configFilePath);
 			try {
@@ -53,17 +37,25 @@ public class Main {
 						configuration.getUserpassword()
 					);
 				Statement sqlStatement = connection.createStatement();
-				String query = "SELECT * from ACC_FDB.AVK_FDB_T_KH_OPS sample 10;";
-				System.out.println("Executing query...");
-				ResultSet result = sqlStatement.executeQuery(query);
-				
-				System.out.println("Processing results...");
-				ResultProcessor resultProcessor = new CleanRowsResultProcessor();
-				Collection<String[]> cleanRows = resultProcessor.ProcessResults(convertResultSet(result));
-				
-				System.out.println("Writing results...");
+				Collection<String> queries = createQueries(configuration.getDatabaseQueryConfiguration());
 				ResultWriter writer = new CsvWriter();
-				writer.Write(cleanRows, "dummyout.csv");
+				int debugBreak = 0;
+				for(String query : queries)
+				{
+					if(debugBreak++ == 2) break; //only run 3 queries for testing
+					System.out.println("Executing query...\n"+query);
+					ResultSet result = sqlStatement.executeQuery(query);
+					
+					System.out.println("Processing results...");
+					ResultProcessor resultProcessor = new CleanRowsResultProcessor();
+					Collection<String[]> cleanRows = resultProcessor.ProcessResults(convertResultSet(result));
+					
+					System.out.println("Writing results...");
+					
+					writer.Write(cleanRows, String.format("out/query_%s.csv", query));
+				}
+				//String query = "SELECT * from ACC_FDB.AVK_FDB_T_KH_OPS sample 10;";
+				
 				sqlStatement.close();
 				connection.close();
 				System.out.println("Done.");
@@ -118,4 +110,33 @@ public class Main {
 		return names;
 	}
 
+	/**
+	 * Creates the queries to get the data
+	 * @param queryConfig
+	 * @return a collection of sql strings
+	 */
+	private static Collection<String> createQueries(DatabaseQueryConfiguration queryConfig)
+	{
+		Collection<String> queries = new ArrayList<String>();
+		String sqlFormat = "SELECT * FROM %s.%s WHERE Bezugsjahr=%d AND FLAG_DEMO=1";
+		//include start and end year
+		int delta = queryConfig.getEndYear() - queryConfig.getStartYear() + 2;
+		int[] years = new int[delta];
+		for(int i = 0; i < delta; i++)
+		{
+			years[i] = queryConfig.getStartYear() + i;
+		}
+		for(String db : queryConfig.getDatabaseNames())
+		{
+			for(String view : queryConfig.getViews(db))
+			{
+				for(int year : years)
+				{
+					queries.add(String.format(sqlFormat, db, view, year));
+				}
+			}
+		}
+		
+		return queries;
+	}
 }

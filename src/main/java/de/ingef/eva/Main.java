1e61c92d7714baa122cbf874463c02dbcf351aa3
MW <martin.wettig@ingef.de>
2017-02-17 13:37:30 +0100

    clean up main

--- src/main/java/de/ingef/eva/Main.java
+++ src/main/java/de/ingef/eva/Main.java
@@ -1,21 +1,21 @@
 package de.ingef.eva;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Collection;
-
 import de.ingef.eva.configuration.Configuration;
 import de.ingef.eva.configuration.ConfigurationReader;
 import de.ingef.eva.configuration.DatabaseEntry;
 import de.ingef.eva.configuration.JsonConfigurationReader;
 import de.ingef.eva.processor.RemoveNewlineCharacters;
 import de.ingef.eva.processor.Processor;
+import de.ingef.eva.processor.RemoveBoundaryWhitespaces;
 import de.ingef.eva.utility.Helper;
-import de.ingef.eva.writer.CsvWriter;
-import de.ingef.eva.writer.ResultWriter;
 
 public class Main {
 
@@ -36,8 +36,9 @@ public class Main {
 					);
 				
 				int[] years = Helper.extractYears(configuration.getDatabaseQueryConfiguration());
-				ResultWriter writer = new CsvWriter();
-				Processor resultProcessor = new RemoveNewlineCharacters();
+				
+				Processor<StringBuilder> newlineRemover = new RemoveNewlineCharacters();
+				Processor<StringBuilder> whitespaceTrimmer = new RemoveBoundaryWhitespaces(); 
 				int debugBreak = 0;
 				long start = System.nanoTime();
 	
@@ -56,31 +57,46 @@ public class Main {
 							System.out.println("Executing query...\n"+query);
 							ResultSet result = preparedStatement.executeQuery();
 							
+							String part = String.format(".part%d.", pid++);
+							String outfileName = String.format("%s/out/query_%s_%s%s.csv", workingDirectory, dbe.getName(), table, part);
+							BufferedWriter writer = new BufferedWriter(new FileWriter(outfileName));
+							System.out.println(String.format("Creating result file %s...", outfileName));
 							while(result.next())
 							{
-								//TODO process column entries immediately
-								System.out.println("Processing results...");
-//								rows = resultProcessor.process(rows);
-								System.out.println("Writing results...");
-								String part = String.format(".part%d.", pid++);
-//								writer.Write(rows, String.format("%s/out/query_%s_%s%s.csv", workingDirectory, dbe.getName(), table, part));
+								//process column entries immediately
+								StringBuilder row = new StringBuilder();
+								int columnCount = result.getMetaData().getColumnCount();
+								for(int i = 1; i <= columnCount; i++)
+								{
+									StringBuilder entry = new StringBuilder(result.getString(i));
+									entry = newlineRemover.process(entry);
+									entry = whitespaceTrimmer.process(entry);
+									row.append(entry);
+									if(i != columnCount)
+									{
+										row.append(";");
+									}
+								}								
+								writer.write(row.toString());
+								writer.newLine();
 							}
-							System.out.println(String.format("Time taken: %d min.", Helper.NanoSecondsToMinutes(System.nanoTime() - queryTimeTaken)));
-							
+							writer.close();
+							System.out.println(String.format("\tDone in %d min.", Helper.NanoSecondsToMinutes(System.nanoTime() - queryTimeTaken)));
 							result.close();
 						}
 						preparedStatement.close();
 					}
 				}
 			
-				//String query = "SELECT * from ACC_FDB.AVK_FDB_T_KH_OPS sample 10;";
-				System.out.println("Time taken (ns)" + (System.nanoTime() - start));
+				System.out.println(String.format("Time taken: %d min.", Helper.NanoSecondsToMinutes(System.nanoTime() - start)));
 				connection.close();
 				System.out.println("Done.");
 			}
 			catch(ClassNotFoundException cnfe){
 				System.out.println("Error: Could not load database driver: " + cnfe.getMessage());
 			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 

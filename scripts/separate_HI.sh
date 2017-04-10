d13e3f7fd52fa0a28e0cfe2cddc7aa28e50c2cf4
MW <martin.wettig@ingef.de>
2017-04-10 14:20:19 +0200

    add HI separation shell script

---
+++ scripts/separate_HI.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# This script extracts rows for the given health insurances.
+# For each health insurance a separate folder is created.
+# This folder contains the corresponding subsets for each ADB table.
+
+# define hi as a map
+typeset -A hi
+hi=(["Bosch"]="108036123" ["Salzgitter"]="101922757")
+
+# read user arguments
+inDirectory="$1"
+outDirectory="$2"
+archiveDirectory=$inDirectory/archive
+
+# check paramters
+if [ $# -ne 2 ] || [ -z $inDirectory ] || [ -z $outDirectory ]; then
+  echo "Invalid arguments. Call $0 <inDirectory> <outDirectory>."
+  exit
+fi
+
+# create directories if necessary
+if [ ! -d $outDirectory ]; then
+  mkdir $outDirectory
+fi
+
+if [ ! -d $archiveDirectory ]; then
+  mkdir $archiveDirectory
+fi
+
+# for each file in the input directory
+for t in $(ls $inDirectory)
+do
+  ifile=$inDirectory/$t
+  # skip directories
+  if [ -d $ifile ]; then
+    continue
+  fi
+  echo "Processing $ifile..."
+  
+  # extract the header of the table
+  header=`head -n 1 $inDirectory/$t`
+  
+  # for each health insurance in the map
+  for k in "${!hi[@]}"
+  do
+    subOutDirectory=$outDirectory/$k
+    # create insurance folder if needed
+    if [ ! -d $subOutDirectory ]; then
+        mkdir $subOutDirectory
+    fi
+    ofile=$subOutDirectory/$t
+    
+	# get the row for the health incurance's h2ik
+	grep "${hi[$k]}" $inDirectory/$t > $ofile
+    
+	# add header
+	sed -i 1i$header $ofile
+  done
+  
+  # compress table file
+  # and move to archive folder
+  gzip $ifile
+  mv $ifile.gz $archiveDirectory/$t.gz
+done
+

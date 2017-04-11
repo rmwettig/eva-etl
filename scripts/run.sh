ea43b315a6e8756ed6ab3d0feab2ba1b4dc2c5fd
MW <martin.wettig@ingef.de>
2017-04-11 16:26:56 +0200

    add linux start script

---
+++ scripts/run.sh
@@ -0,0 +1,17 @@
+adbConfig=""
+fdbConfig=""
+spkConfig=""
+
+
+java -jar eva-data.jar $adbConfig makejob &&
+	fexp < -e /logs/dump-adb.err < eva-dump-adb.fx &&
+	java -jar eva-data.jar $adbConfig &
+	
+java -jar eva-data.jar $fdbConfig makejob &&
+	fexp < -e /logs/dump-fdb.err < eva-dump-fdb.fx &&
+	java -jar eva-data.jar $fdbConfig &
+	
+java -jar eva-data.jar $spkConfig makejob &&
+	fexp < -e /logs/dump-spk.err < eva-dump-spk.fx &&
+	java -jar eva-data.jar $spkConfig &&
+	java -jar eva-data.jar $spkConfig map &

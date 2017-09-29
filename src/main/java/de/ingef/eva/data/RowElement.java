5feeedb225c2989b8dfd107b1585ae4ec564b135
MW <martin.wettig@ingef.de>
2017-09-29 13:54:38 +0200

    add cast for content

--- src/main/java/de/ingef/eva/data/RowElement.java
+++ src/main/java/de/ingef/eva/data/RowElement.java
@@ -1,8 +1,13 @@
 package de.ingef.eva.data;
 
 public interface RowElement {
+
+	@Deprecated
 	public int getIndex();
+	@Deprecated
 	public String getName();
 	public String getContent();
 	public TeradataColumnType getType();
+	
+	public <T> T asType(Class<T> type) throws ClassCastException;
 }

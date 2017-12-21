c7a3877ae4ddb435b01104617ff9bb0c2e2345b9
MW <martin.wettig@ingef.de>
2017-12-21 11:48:27 +0100

    remove FastExport based scripts

--- scripts/linux/adb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Call this from the dump root folder
-
-java -jar eva-data.jar -makejob P:\eva-dump-configs\production\dump-adb.json
-fexp -e logs/dump-fdb-destatis.err < P:\eva-dump-configs\production\fexp-jobs\eva-dump-adb.fx
-java -jar eva-data.jar -clean P:\eva-dump-configs\production\dump-adb.json
-. /p/workspace/projects/eva-data/scripts/remove_duplicated_kv.sh out/ACC_ADB/ACC_ADB_AVK_ADB_T_Arzt_GOP.csv
-. /p/workspace/projects/eva-data/scripts/separate_HI.sh out/ACC_ADB out/ACC_ADB
--- scripts/linux/fdb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Call this from the dump root folder
-
-java -jar eva-data.jar -makejob P:\eva-dump-configs\production\dump-fdb-destatis.json
-fexp -e logs/dump-fdb-destatis.err < P:\eva-dump-configs\production\fexp-jobs\eva-dump-fdb-destatis.fx
-java -jar eva-data.jar -clean P:\eva-dump-configs\production\dump-fdb-destatis.json
-. /p/workspace/projects/eva-data/scripts/remove_duplicated_kv.sh out/ACC_FDB/ACC_FDB_AVK_FDB_T_Arzt_GOP.csv

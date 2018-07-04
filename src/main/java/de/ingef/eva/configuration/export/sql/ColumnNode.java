89ca88f67c32c80aa1e8a4698afa23b2c8b7e7ef
MW <martin.wettig@ingef.de>
2018-07-04 10:24:12 +0200

    remove unused alias field

--- src/main/java/de/ingef/eva/configuration/export/sql/ColumnNode.java
+++ src/main/java/de/ingef/eva/configuration/export/sql/ColumnNode.java
@@ -18,23 +18,9 @@ public class ColumnNode extends SqlNode {
 	 * Name of the column
 	 */
 	private final String name;
-	/**
-	 * @deprecated unused
-	 */
-	private final String alias;
-	
+
 	public ColumnNode(String columnName) {
-		this(columnName, "");
-	}
-	
-	/**
-	 * @deprecated unused
-	 * @param columnName
-	 * @param columnAlias
-	 */
-	public ColumnNode(String columnName, String columnAlias) {
 		super(SqlNodeType.COLUMN);
 		name = columnName;
-		alias = columnAlias;
 	}
 }
